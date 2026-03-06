fce4121ce03aac6333af63052920c2de54beaf70
Umer Ali <mrumerali446@gmail.com>
2026-03-06 11:28:35 -0800

    vercel

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 from flask import Flask, render_template
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
+
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
 
 @app.route("/")
 def home():
     return render_template("index.html")
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
